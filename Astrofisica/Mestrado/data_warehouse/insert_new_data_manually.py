45a3c7ff4f7311d691d39805bc79fdafa12b184a
Thiago Laidler <thiagolaidler@gmail.com>
2025-04-21 12:54:03 -0300

    Pause insert_new_data dev due to lack of light curves data at portals

---
+++ Astrofisica/Mestrado/data_warehouse/insert_new_data_manually.py
@@ -0,0 +1,4 @@
+#Devido aos problemas com o "insert_new_data.py", foi necessário criar um script para inserir os dados manualmente 
+#Esse script serve apenas para inserir os dados na DB utilizando as curvas de luz obtidas no drive do Grupo do Rio
+
+
